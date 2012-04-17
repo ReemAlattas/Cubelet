458038fcd700990c1ef29d29c96d866afb416c16
Andy McEvoy <andymcevoy@Correll4.(none)>
2012-04-17 14:53:34 -0600

    Initial posting of Cubelet files.
    This collection of files is intended to be used with ROS.  Files allow
    cubelets to be used in ROS simulations with realistic behaviors.

---
+++ build/CMakeFiles/Cubelet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Cubelet.dir/src/modelinsert.o"
+  "../lib/libCubelet.pdb"
+  "../lib/libCubelet.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Cubelet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
